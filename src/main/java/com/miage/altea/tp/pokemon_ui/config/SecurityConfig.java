76d35f47e623da09c28930b0e117a6ce6555fefd
Paul <paul_letailleur@hotmail.com>
2019-03-11 12:03:03 +0100

    Suppression coquille + ajout debut securite

---
+++ src/main/java/com/miage/altea/tp/pokemon_ui/config/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.miage.altea.tp.pokemon_ui.config;
+
+
+import com.miage.altea.tp.pokemon_ui.service.TrainerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collection;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    TrainerService trainerService;
+
+
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    public UserDetailsService userDetailsService() {
+        UserDetailsService service = new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+                return new UserDetails() {
+                    @Override
+                    public Collection<? extends GrantedAuthority> getAuthorities() {
+                        return null;
+                    }
+
+                    @Override
+                    public String getPassword() {
+                        return null;
+                    }
+
+                    @Override
+                    public String getUsername() {
+                        return null;
+                    }
+
+                    @Override
+                    public boolean isAccountNonExpired() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean isAccountNonLocked() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean isCredentialsNonExpired() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean isEnabled() {
+                        return false;
+                    }
+                }
+            }
+        };
+    }
+}
